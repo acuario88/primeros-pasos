d7a4d102e0004fffe75d57a63841ea34b6c0c006
acuario88 <123593646+acuario88@users.noreply.github.com>
2023-01-30 09:52:22 -0300

    Add files via upload

---
+++ Primer proyecto/java/java.js
@@ -0,0 +1,6 @@
+document.getElementById('boton1').onclick = function () { 
+    document.getElementById ("demo1").innerHTML = "1122334455"
+}
+document.getElementById('boton2').onclick = function () { 
+    document.getElementById ("demo2").innerHTML = "albacerrati@gmail.com"
+}
